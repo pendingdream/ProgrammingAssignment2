8e6a42c75c174c9620cabff16762d70443ab2ef1
pendingdream <wumengfei_emma@hotmail.com>
2014-04-21 02:12:58 +0800

    finish assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## For Coursera Data Science :R programming, Assignment 2
+## Use cached information to reduce calculaton for the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix will construct a speciel matrix which is really a list containing a function to
+## 1.set the matrix
+## 2.get the matrix
+## 3.set the inverse of the matrix
+## 4.get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates the inversion matrix of the special 
+## matrix created with the above function. 
+## 1.First checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the matrix and put it in the cache.
+## Error messages will be shown if the matrix is not invertible
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data...")
+                return(inv)
+        }
+        data <- x$get()
+        if (!nrow(data) == ncol(data)) {
+                stop("Not Square Matrix")
+        }
+        inv <- try(solve(data, diag(nrow(data))),silent=TRUE)
+        if(class(inv) == "try-error") {
+                stop("Not Invertible")
+        }
+        x$setinv(inv)
+        inv
 }
